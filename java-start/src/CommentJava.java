38982b976d53358a4d42b42155c4378e4771b6e4
mok010 <blueangle1004@gmail.com>
2024-02-18 20:16:00 +0900

    20240218

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,9 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java"); //이건 주석이지롱
+        //System.out.println("hello java");
+
+        /*System.out.println("hello java");
+        System.out.println("hello java");*/
+    }
+}
---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a); 아예 출력 불가
+    }
+}
