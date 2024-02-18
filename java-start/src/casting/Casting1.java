a19ac93bcb8d822b4b4594b90cf16f7001aec750
mok010 <blueangle1004@gmail.com>
2024-02-18 22:15:20 +0900

    today's study over

---
+++ java-start/src/casting/Casting1.java
@@ -0,0 +1,9 @@
+package casting;
+
+public class Casting1 {
+    public static void main(String[] args) {
+        int intValue = 10;
+        long longValue;
+        double doubleValue;
+    }
+}
--- java-start/src/scope/Scope1.java
+++ java-start/src/scope/Scope1.java
@@ -2,6 +2,15 @@ package scope;
 
 public class Scope1 {
     public static void main(String[] args) {
-        int m = 10;
+        int m = 10; //m 생존시작
+        if (true) {
+            int x = 20; //x 생존시작
+            System.out.println("if m = " + m);
+            System.out.println("if x = " + x);
+        }
+        System.out.println("if m = " + m);
+        //System.out.println("if x = " + x);
+        //지역변수 스코프 이탈 (선언된 코드 블록 {})
+        //스코프를 사용하면 코드의 복잡도를 줄일 수 있다.
     }
 }
